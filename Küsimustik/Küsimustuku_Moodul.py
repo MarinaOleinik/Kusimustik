603fe7dd8934aec7077ffcd3336589634bff3e30
marinaoleinik <marina.oleinik@tthk.ee>
2026-01-16 09:17:28 +0200

    Add project files.

---
+++ Küsimustik/Küsimustuku_Moodul.py
@@ -0,0 +1,13 @@
+def andmete_lugemine_failidest():
+    # Siin võiks olla kood, mis loeb andmeid failidest
+    pass
+
+def andmete_salvestamine_failidesse():
+    # Siin võiks olla kood, mis salvestab andmeid failidesse
+    pass
+
+def testimine():
+    # Siin võiks olla kood, mis teostab testimist
+    pass
+def emaili_saatmine():
+    # Siin võiks olla kood, mis saadab e-kirju
